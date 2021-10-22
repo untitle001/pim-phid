9ace7cc72830f18d328218daa6e30d88e674c295
Erth <ertherthp@gmail.com>
2021-10-22 16:26:28 +0700

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange:{
+          100: '#F6E8D5',
+          200: '#F3E0C8',
+          500: '#E4B781',
+          700: '#D1892E',
+        },
+        ocean:{
+          700: '#266573'
+        },
+        "red-orange":{
+          500: '#D95426'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
